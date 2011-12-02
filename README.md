82b57b4231908ea2ef3e1eb17de5462ec57c1b41
Marc Grabanski <m@marcgrabanski.com>
2011-12-02 13:32:28 -0600

    initial commit of hisrc jQuery plugin

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple jQuery plugin to replace default (low resolution images) with larger images based on browser width.
+
+```html
+<img src='http://placehold.it/400x200.png' data-hisrc='http://placehold.it/800x200.png'>
+```
---
+++ hisrc.js
@@ -0,0 +1,37 @@
+(function($){
+	$.hisrc = {
+		els: $(),
+		init: false
+	}
+	
+	$.hisrc.defaults = {
+		minwidth: 640
+	}
+	
+	$.fn.hisrc = function(options) {
+		var settings = $.extend({}, $.hisrc.defaults, options);
+		
+		$.hisrc.els = $.hisrc.els.add(this);
+		
+		if (!$.hisrc.init) {
+			$(window).on('resize.hisrc', function(){
+				$.hisrc.els.trigger('swapres.hisrc');
+			});
+		}
+		
+		return this.each(function(){
+			$(this).data('lowsrc', $(this).attr('src'));
+			
+			$(this)
+				.on('swapres.hisrc', function(){
+					if ($(window).width() > settings.minwidth) {
+						$(this).attr('src', $(this).data('hisrc'));
+					} else {
+						$(this).attr('src', $(this).data('lowsrc'));
+					}
+				})
+				.trigger('swapres.hisrc');
+		})
+			
+	}
+})(jQuery);
